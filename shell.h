d9ab9560ca4858ba62207488454e7f6b3fae4dc3
abdussamad10 <abdessamadseffir@gmail.com>
2023-08-23 06:45:28 -0700

    cc

--- shell.h
+++ shell.h
@@ -397,9 +397,9 @@ list_t *add_node(list_t **, const char *, int);
 
 list_t *add_node_end(list_t **, const char *, int);
 
-size_t print_list_str(const list_t *);
+size_t print_list(const list_t *);
 
-int delete_node_at_index(list_t **, unsigned int);
+int delete_node_at(list_t **, unsigned int);
 
 void free_list(list_t **);
 
